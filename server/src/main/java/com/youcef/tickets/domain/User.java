6387464213548fa2ffc512cd589ecfbb669fba24
Youcef <y_zemirline@hotmail.com>
2025-06-04 15:05:11 +0100

    JWT to User

--- server/src/main/java/com/youcef/tickets/domain/User.java
+++ server/src/main/java/com/youcef/tickets/domain/User.java
@@ -14,7 +14,6 @@ import java.util.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
 
     @Id
---
+++ server/src/main/java/com/youcef/tickets/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.youcef.tickets.repositories;
+
+import com.youcef.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
